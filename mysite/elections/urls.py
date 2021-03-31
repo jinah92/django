2ae1a9d8af5c1754f4401f56704028156cfee138
jinah <jinah0818@gmail.com>
2021-03-31 21:55:09 +0900

    여론조사 모델 및 템플릿 구현

--- mysite/elections/urls.py
+++ mysite/elections/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from .views import index
+from .views import index, areas, polls
 
 urlpatterns = [
     path('', index),
+    path('areas/<str:area>/', areas),
+    path('polls/<int:poll_id>/', polls)
 ]
